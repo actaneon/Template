283fbf0cc9697af4e65370e5b79c185b5a5f292d
Josh King <actaneon@gmail.com>
2012-08-08 19:03:58 -0700

    Created Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomage::Application.config.secret_token = 'df67404a690beee267f0fe8d6407fd8eac05766cd62a50430d5558e9c6b34d28642f88e5b3f92e9682e7f3438a5395211ffadd60bb7476d20ab55d5681d854c2'
